b1b61f195519f43fbc0069829dda0a2f1d391c01
付薛龙 <fuxuelong@fuxuelongdeMacBook-Pro.local>
2018-06-23 15:05:19 +0800

    3

---
+++ auth-service/src/main/java/com/cnhtcqk/authservice/dao/UserDao.java
@@ -0,0 +1,8 @@
+package com.cnhtcqk.authservice.dao;
+
+import com.cnhtcqk.authservice.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserDao extends JpaRepository<User,Long> {
+    User findByUesrname(String username);
+}
---
+++ auth-service/src/main/java/com/cnhtcqk/authservice/service/security/UserServiceDetail.java
@@ -0,0 +1,19 @@
+package com.cnhtcqk.authservice.service.security;
+
+import com.cnhtcqk.authservice.dao.UserDao;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserServiceDetail implements UserDetailsService {
+
+    @Autowired
+    private UserDao userRepository;
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return userRepository.findByUesrname(username);
+    }
+}
